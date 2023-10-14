6639b97621e2934dc3731878196a5a12e5b03f17
Dani John <rocksdanister@gmail.com>
2023-10-14 08:14:07 +0530

    Show day number in weekday selector

--- src/Drizzle.UI.UWP/Converters/DateTimeToDayConverter.cs
+++ src/Drizzle.UI.UWP/Converters/DateTimeToDayConverter.cs
@@ -1,30 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Windows.UI.Xaml.Data;
-using Drizzle.Weather.Helpers;
 
-namespace Drizzle.UI.UWP.Converters;
-
-public class DateTimeToDayConverter : IValueConverter
+namespace Drizzle.UI.UWP.Converters
 {
-    public object Convert(object value, Type targetType, object parameter, string language)
+    public class DateTimeToDayConverter : IValueConverter
     {
-        try
+        public object Convert(object value, Type targetType, object parameter, string language)
         {
-            return ((DateTime)value).DayOfWeek.GetDayName((parameter as string ?? string.Empty).Equals("Short"));
+            return ((DateTime)value).Day;
         }
-        catch
-        {
-            return "Error";
-        }
-    }
 
-    public object ConvertBack(object value, Type targetType, object parameter, string language)
-    {
-        throw new NotImplementedException();
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
---
+++ src/Drizzle.UI.UWP/Converters/DateTimeToDayNameConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+using Drizzle.Weather.Helpers;
+
+namespace Drizzle.UI.UWP.Converters;
+
+public class DateTimeToDayNameConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, string language)
+    {
+        try
+        {
+            return ((DateTime)value).DayOfWeek.GetDayName((parameter as string ?? string.Empty).Equals("Short"));
+        }
+        catch
+        {
+            return "Error";
+        }
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, string language)
+    {
+        throw new NotImplementedException();
+    }
+}
